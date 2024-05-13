eb8747f36b9fbbec86f99e7b0de0db3a9ef0b067
Alexei Naraev <marlausor@gmail.com>
2024-05-13 21:00:05 +0300

    LoadImage.groovy

---
+++ src/LoadImage.groovy
@@ -0,0 +1,10 @@
+
+
+def load_image(String image_name, String username, String password) {
+    assert (image_name && username && password)
+    sh """
+    buildah login -u ${username} -p ${password} docker.io
+    buildah push ${username}/${imagename}
+    buildah logout docker.io
+    """
+}

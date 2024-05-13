14693dace6d80f0d99de6670bc8ec4b198e20fb7
Alexei Naraev <marlausor@gmail.com>
2024-05-13 21:11:06 +0300

    function in loadImage reneamed

--- src/LoadImage.groovy
+++ src/LoadImage.groovy
@@ -1,6 +1,6 @@
 
 
-def load_image(String image_name, String username, String password) {
+def call(String image_name, String username, String password) {
     assert (image_name && username && password)
     sh """
     buildah login -u ${username} -p ${password} docker.io

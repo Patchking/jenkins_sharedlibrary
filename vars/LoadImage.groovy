0c4edcbe3337e4980ae97a665f0a09f316228ff3
Alexei Naraev <marlausor@gmail.com>
2024-05-14 15:08:41 +0300

    LoadImage rewrited

--- vars/LoadImage.groovy
+++ vars/LoadImage.groovy
@@ -1,10 +1,19 @@
 
 
-def call(String image_name, String username, String password) {
-    assert (image_name && username && password)
-    sh """
-    buildah login -u ${username} -p ${password} docker.io
-    buildah push ${username}/${imagename}
-    buildah logout docker.io
-    """
+def call(String image_name, String credential) {
+    // assert (image_name && username && password)
+    withCredentials([
+        usernamePassword(
+            credentialsId: credential,
+            usernameVariable: "REGESTRY_LOGIN",
+            passwordVariable: "REGESTRY_PASSWORD"
+        )  
+    ]) {
+        sh """
+        buildah login -u ${REGESTRY_LOGIN} -p ${REGESTRY_PASSWORD} docker.io
+        buildah push ${REGESTRY_LOGIN}/${imagename}
+        buildah logout docker.io
+        """
+    }
+
 }
